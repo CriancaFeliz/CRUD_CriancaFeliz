a75c7e43aae8c98801e928a139b0fdbe001aa43b
Andre Souza <andre.cassulino@gmail.com>
2026-09-08 19:48:49 -0300

    fix(docker): update schema init path to schema_completo.sql

--- docker/mysql/01-init.sh
+++ docker/mysql/01-init.sh
@@ -4,4 +4,4 @@ set -e
 echo "Importing Crianca Feliz schema..."
 
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" "${MYSQL_DATABASE}" \
-    < /docker-init/SETUP_COMPLETO_FINAL.sql
+    < /docker-init/schema_completo.sql
